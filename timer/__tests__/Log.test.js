23322cd6147066b40ea7627b8742899e729e64a0
Andrew Pryshchepa <vinglfm@gmail.com>
2018-02-19 22:38:45 +0000

    test for Log component

---
+++ timer/__tests__/Log.test.js
@@ -0,0 +1,13 @@
+const React = require('react');
+const { createRenderer } = require('react-test-renderer/shallow');
+const Log = require('../src/Log.jsx');
+
+test('Log renders props.data', () => {
+
+  const logRenderer = createRenderer();
+  logRenderer.render(<Log data="for rendering"/>);
+  let log = logRenderer.getRenderOutput();
+
+  expect(log.type).toBe('p');
+  expect(log.props.children).toBe("for rendering");
+});
